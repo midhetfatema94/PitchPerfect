a291ec96d012692a641b725e13b270beb2e954ac
Midhet <midhetfatema94@gmail.com>
2016-04-07 15:02:32 +0530

    initial commit

---
+++ Pitch Perfect/RecorderController.swift
@@ -0,0 +1,28 @@
+//
+//  RecorderController.swift
+//  Pitch Perfect
+//
+//  Created by Chintan Shah on 22/02/16.
+//  Copyright © 2016 Chintan Shah. All rights reserved.
+//
+
+import UIKit
+
+class RecorderController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func playSlowAudio(sender: UIButton) {
+        // TODO: play slow audio
+    }
+    
+    @IBAction func playFastAudio(sender: UIButton) {
+        // TODO: play fast audio
+    }
+}
