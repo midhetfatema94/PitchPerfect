da01912a037dcfeb547765f00e015ecf17d6fad5
Midhet Sulemani <midhetsulemani@Midhets-MacBook-Air.local>
2016-11-01 16:18:16 +0300

    almost done

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,59 @@
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var darthVaderButton: UIButton!
+    @IBOutlet weak var parrotButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioUrl: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int { case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        
+        print("playing sound")
+        switch ButtonType(rawValue: sender.tag)! {
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(rate: 1000)
+        case .Vader:
+            playSound(rate: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(playState: .Playing)
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        
+        print("playing sound stopped")
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(playState: .NotPlaying)
+    }
+    
+}
---
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,86 @@
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stopButton.isEnabled = false
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        
+        print("recording done!")
+        
+        if flag {
+            
+            self.performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        }
+        else {
+            
+            print("recording failed")
+        }
+        
+    }
+    
+    func startRecording() {
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURL(withPathComponents: pathArray)
+        print(filePath!)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            playSoundsVC.recordedAudioUrl = sender as! NSURL
+            
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordAudio(_ sender: UIButton) {
+        
+        recordingInProgress.isHidden = false
+        stopButton.isEnabled = true
+        recordButton.isEnabled = false
+        
+        startRecording()
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        
+        recordingInProgress.isHidden = true
+        recordButton.isEnabled = true
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+}
+
